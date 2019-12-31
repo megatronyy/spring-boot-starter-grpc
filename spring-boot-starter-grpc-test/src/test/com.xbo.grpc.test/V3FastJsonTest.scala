9825d64a38f2e2c836fa42bf1878fc20d09f70cb
屈文斌 <quwenbin@blchina.com>
2019-12-31 16:44:25 +0800

    换包名

--- spring-boot-starter-grpc-test/src/test/com.xbo.grpc.test/V3FastJsonTest.scala
+++ spring-boot-starter-grpc-test/src/test/com.xbo.grpc.test/V3FastJsonTest.scala
@@ -1,4 +1,4 @@
-package com.anoyi.grpc.test
+package com.xbo.grpc.test
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
@@ -6,7 +6,7 @@ import io.gatling.http.Predef._
 class V3FastJsonTest extends Simulation {
 
   // 测试数据
-  val data = "{\"id\":1,\"name\":\"anoyi\",\"age\":20,\"gender\":\"男\",\"scores\":{\"语文\":99,\"数学\":20},\"friend\":{\"id\":2,\"name\":\"乖乖\",\"age\":21,\"gender\":\"女\",\"scores\":{\"语文\":10,\"数学\":98},\"friend\":null,\"pet\":null,\"listValue\":[]},\"pet\":{\"type\":\"龙\",\"name\":\"青尊\",\"owner\":{\"id\":1,\"name\":\"anoyi\",\"age\":20,\"gender\":\"男\",\"scores\":null,\"friend\":null,\"pet\":null,\"listValue\":null}},\"listValue\":[1,\"哇哦\"]}"
+  val data = "{\"id\":1,\"name\":\"xbo\",\"age\":20,\"gender\":\"男\",\"scores\":{\"语文\":99,\"数学\":20},\"friend\":{\"id\":2,\"name\":\"乖乖\",\"age\":21,\"gender\":\"女\",\"scores\":{\"语文\":10,\"数学\":98},\"friend\":null,\"pet\":null,\"listValue\":[]},\"pet\":{\"type\":\"龙\",\"name\":\"青尊\",\"owner\":{\"id\":1,\"name\":\"anoyi\",\"age\":20,\"gender\":\"男\",\"scores\":null,\"friend\":null,\"pet\":null,\"listValue\":null}},\"listValue\":[1,\"哇哦\"]}"
 
   // 请求数
   val maxCount = 100000
